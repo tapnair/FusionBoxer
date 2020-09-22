8412a4b5d8da84139dfc55595f164c5dd1725deb
Patrick Rainsberry <patrick.rainsberry@autodesk.com>
2020-09-22 16:04:57 -0700

    cleanup

--- commands/OffsetBoundingBoxCommand.py
+++ commands/OffsetBoundingBoxCommand.py
@@ -23,7 +23,6 @@ def mid_point(p1: adsk.core.Point3D, p2: adsk.core.Point3D):
 
 class Direction:
     def __init__(self, name: str, direction: adsk.core.Vector3D, inputs: adsk.core.CommandInputs):
-
         ao = AppObjects()
         self.name = name
         self.direction = direction
@@ -36,7 +35,6 @@ class Direction:
             default_shell = f"1 {units}"
 
         default_value = adsk.core.ValueInput.createByString(default_shell)
-
         self.dist_input: adsk.core.DistanceValueCommandInput = inputs.addDistanceValueCommandInput(
             f"dist_{self.name}", self.name, default_value
         )
@@ -57,9 +55,7 @@ class TheBox:
     def __init__(self, b_box: adsk.core.BoundingBox3D, inputs: adsk.core.CommandInputs):
         ao = AppObjects()
         root_comp = ao.root_comp
-
         self.b_box = b_box
-
         self.modified_b_box = self.b_box.copy()
 
         self.x_pos_vector = root_comp.yZConstructionPlane.geometry.normal.copy()
@@ -82,7 +78,6 @@ class TheBox:
         }
 
         self.graphics_group = ao.root_comp.customGraphicsGroups.add()
-
         self.brep_mgr = adsk.fusion.TemporaryBRepManager.get()
         self.brep_box = None
         self.graphics_box = None
@@ -94,7 +89,6 @@ class TheBox:
         self.modified_b_box.expand(point)
 
     def update_manipulators(self):
-
         min_p = self.modified_b_box.minPoint
         max_p = self.modified_b_box.maxPoint
 
@@ -124,24 +118,17 @@ class TheBox:
         self.clear_graphics()
         self.update_brep_box()
 
-        self.graphics_box = self.graphics_group.addBRepBody(self.brep_box)
-
-        # self.graphics_group.setOpacity(.5, True)
         color = adsk.core.Color.create(10, 200, 50, 125)
         color_effect = adsk.fusion.CustomGraphicsSolidColorEffect.create(color)
+        self.graphics_box = self.graphics_group.addBRepBody(self.brep_box)
         self.graphics_box.color = color_effect
-
-        # self.graphics_box.transform = transform
 
     def clear_graphics(self):
         if self.graphics_box is not None:
-            # self.graphics_box = adsk.fusion.CustomGraphicsBRepBody.cast(self.graphics_box)
             if self.graphics_box.isValid:
                 self.graphics_box.deleteMe()
 
     def update_brep_box(self):
-        # if self.brep_box is not None:
-        #     self.brep_box.deleteMe()
         o_box = adsk.core.OrientedBoundingBox3D.create(
             mid_point(self.modified_b_box.minPoint, self.modified_b_box.maxPoint),
             self.x_pos_vector,
@@ -151,13 +138,6 @@ class TheBox:
             self.modified_b_box.maxPoint.z - self.modified_b_box.minPoint.z
         )
         self.brep_box = self.brep_mgr.createBox(o_box)
-
-        # transform = adsk.core.Matrix3D.create()
-        # center_point = mid_point(self.b_box.minPoint, self.b_box.maxPoint)
-        # vector = center_point.asVector()
-        # vector.scaleBy(-1)
-        # transform.translation = vector
-        # self.brep_mgr.transform(self.brep_box, transform)
 
     def make_brep_real(self, thickness):
         ao = AppObjects()
@@ -191,20 +171,17 @@ class TheBox:
         if ao.design.designType == adsk.fusion.DesignTypes.ParametricDesignType:
             apper.end_group(start)
 
+
 class OffsetBoundingBoxCommand(apper.Fusion360CommandBase):
     def __init__(self, name: str, options: dict):
         self.the_box: TheBox
         self.the_box = None
         super().__init__(name, options)
 
-    def on_preview(self, command: adsk.core.Command, inputs: adsk.core.CommandInputs, args: adsk.core.CommandEventArgs, input_values):
-        ao = AppObjects()
-
+    def on_preview(self, command, inputs, args, input_values):
         selections = input_values['body_select']
-
         if len(selections) > 0:
             new_box: adsk.core.BoundingBox3D = selections[0].boundingBox
-
             for selection in selections[1:]:
                 new_box.combine(selection.boundingBox)
 
@@ -212,7 +189,6 @@ class OffsetBoundingBoxCommand(apper.Fusion360CommandBase):
 
             direction: Direction
             for key, direction in self.the_box.directions.items():
-
                 point = direction.dist_input.manipulatorOrigin.copy()
                 vector = direction.direction.copy()
                 vector.normalize()
@@ -221,30 +197,14 @@ class OffsetBoundingBoxCommand(apper.Fusion360CommandBase):
 
                 if not self.the_box.modified_b_box.contains(point):
                     self.the_box.update_box(point)
-                    # self.the_box.update_manipulators()
 
             self.the_box.update_graphics()
 
-        # for cmd_input in inputs:
-        #     if cmd_input.objectType == adsk.core.DistanceValueCommandInput.classType():
-        #         cmd_input = adsk.core.DistanceValueCommandInput.cast(cmd_input)
-        #
-        #         point = cmd_input.manipulatorOrigin.copy()
-        #         vector = cmd_input.manipulatorDirection.copy()
-        #         vector.normalize()
-        #         vector.scaleBy(cmd_input.value)
-        #         point.translateBy(vector)
-        #
-        #         if not self.the_box.modified_b_box.contains(point):
-        #             self.the_box.update_box(point)
-
-    def on_input_changed(self, command: adsk.core.Command, inputs: adsk.core.CommandInputs, changed_input,
-                         input_values):
+    def on_input_changed(self, command, inputs, changed_input, input_values):
         if changed_input.id == 'body_select':
             selections = input_values['body_select']
 
             if len(selections) > 0:
-                # TODO Add selections
                 new_box: adsk.core.BoundingBox3D = selections[0].boundingBox
 
                 for selection in selections[1:]:
@@ -252,30 +212,15 @@ class OffsetBoundingBoxCommand(apper.Fusion360CommandBase):
 
                 self.the_box.initialize_box(new_box)
                 self.the_box.update_manipulators()
-            # else:
-            #     direction: Direction
-            #     for key, direction in self.the_box.directions.items():
-            #
-            #         point = direction.origin.copy()
-            #         vector = direction.direction.copy()
-            #         vector.normalize()
-            #         vector.scaleBy(direction.dist_input.value)
-            #         point.translateBy(vector)
-            #
-            #         if not self.the_box.modified_b_box.contains(point):
-            #             self.the_box.update_box(point)
-            #
-            #     self.the_box.update_manipulators()
 
-    def on_execute(self, command: adsk.core.Command, inputs: adsk.core.CommandInputs, args, input_values):
-
+    def on_execute(self, command, inputs, args, input_values):
         self.the_box.clear_graphics()
         self.the_box.make_brep_real(input_values['thick_input'])
 
-    def on_destroy(self, command: adsk.core.Command, inputs: adsk.core.CommandInputs, reason, input_values):
+    def on_destroy(self, command, inputs, reason, input_values):
         self.the_box.clear_graphics()
 
-    def on_create(self, command: adsk.core.Command, inputs: adsk.core.CommandInputs):
+    def on_create(self, command, inputs):
         ao = AppObjects()
 
         selection_input = inputs.addSelectionInput('body_select', "Select Bodies",
@@ -288,7 +233,6 @@ class OffsetBoundingBoxCommand(apper.Fusion360CommandBase):
             default_shell = config.DEFAULT_SHELL
         except AttributeError:
             default_shell = f"1 {units}"
-
         default_value = adsk.core.ValueInput.createByString(default_shell)
         inputs.addValueInput('thick_input', "Outer Shell Thickness", units, default_value)
 
